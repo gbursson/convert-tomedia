da0c1ba63b00ed3a83f6517c61e5d5b74265b9e2
gbursson <gbursson@gmail.com>
2018-10-14 12:52:49 +0100

    Initial commit, Convert-ToMP3 wrapper for ffmpeg

---
+++ tools.psm1
@@ -0,0 +1,27 @@
+﻿<#
+.Synopsis
+   Convert file to mp3 using ffmpeg
+.DESCRIPTION
+   Convert file to mp3 using ffmpeg
+.EXAMPLE
+   Convert-ToMp3 extension filename
+#>
+function ConvertTo-MP3 {
+    Param
+    (
+        # Filename to convert
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        $Filetype,
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
+        $Filename
+
+    )
+
+    $NewFilename = [System.IO.Path]::GetFileNameWithoutExtension($Filename) + "." + ($Filetype)
+    $Command = {ffmpeg -i ($Filename) ($NewFilename)}
+    Invoke-Command $Command
+}
